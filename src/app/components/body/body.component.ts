581b09c65936c3659688149ed4ce8805538651df
Luis Villalobos <luisevm24@outlook.com>
2020-12-31 15:26:13 -0600

    practice structural directives

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class BodyComponent {
 
+    status: boolean = true;
+
+    quote: any = {
+        message: 'I know for sure that what we well on is who we become',
+        author: 'Oprah Winfrey'
+    }
+
+    avengers: string[] = ['Spiderman', 'Thor', 'Hulk', 'Black Panter'];
+
+
 }
