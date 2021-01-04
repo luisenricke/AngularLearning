1c6c2d356d277c31a21c911d480c1bb23510ae23
Luis Villalobos <luisevm24@outlook.com>
2021-01-03 19:18:32 -0600

    pipes: uppercase, lowercase, slice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularLearning';
+
+  username: string = '@LuisEnricKe';
+  numbers: number[] = [1,2,3,4,5,6,7,8,9];
 }
