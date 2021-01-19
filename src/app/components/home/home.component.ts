cdd5738692cb5f9593b965ed5d6977c19974247e
Luis Villalobos <luisevm24@outlook.com>
2021-01-19 16:53:28 -0600

    try simple request of countries

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  countries: any[] = [];
+
+  constructor(private http: HttpClient) {
+    console.log('Home constructor');
+
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe( (response: any) => {
+      this.countries = response;
+      console.log(response);
+    });
+  }
 
   ngOnInit(): void {
   }
